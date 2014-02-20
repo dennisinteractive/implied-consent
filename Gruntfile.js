0322c6a7004d8afe5dec7bf3425958056ed17a4e
Will Howlett <will_howlett@dennis.co.uk>
2014-02-20 18:07:06 +0000

    Case 20921: Fix IE8 issue & configure to use Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,60 @@
+module.exports = function(grunt) {
+  "use strict";
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: [
+         'src/includes/jquery.quarantine.js',
+         'src/includes/jquery.cookie.js',
+         'src/*.js'],
+        dest: 'cookie-notice.js'
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: '/*!\n'+
+                ' * Implied Consent - jQuery Cookie Notice plugin\n'+
+                ' * self-contained version <%= pkg.version %>\n'+
+                ' * \n'+
+                ' * Copyright Dennis Publishing\n'+
+                ' * Released under MIT license\n'+
+                ' * \n'+
+                ' * Display a cookie notice bar at the top of the page and set a cookie to\n'+
+                ' * prevent further display when any local link or the close button is clicked.\n'+
+                ' * <%= pkg.repository.url %>\n'+
+                ' */\n'
+      },
+      dist: {
+        files: {
+          'cookie-notice.min.js': ['<%= concat.dist.dest %>']
+        }
+      },
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'src/cookie-notice.js'],
+      options: {
+        loopfunc: true,
+        strict: true
+      }
+    },
+    watch: {
+      files: ['src/**/*.js'],
+      tasks: ['jshint', 'concat', 'uglify']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('test', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+};
---
+++ src/includes/jquery.quarantine.js
@@ -0,0 +1,75 @@
+/**
+ * jQuery Quarantine - isolate jQuery
+ *
+ * https://github.com/quickredfox/jQuery-Quarantine
+ */
+if(typeof window.jQQ !== 'object') {
+  (function() {
+    "use strict";
+    var callbacks = [], jq;
+
+    function loadScript(url, callback) {
+      var script = document.createElement('script');
+      script.type = "text/javascript";
+
+      if (typeof script.readyState === 'undefined') {
+        script.onload = function() {
+          callback();
+        };
+      } else { // IE LAST!
+        script.onreadystatechange = function() {
+          if (script.readyState === 'loaded' || script.readyState === 'complete') {
+            script.onreadystatechange = null;
+            callback();
+          }
+        };
+      }
+      script.src = url;
+      document.getElementsByTagName("head")[0].appendChild(script);
+      return script;
+    }
+
+    function validateCallback(callback) {
+      if (typeof callback === 'undefined') throw "Cannot validate callback: undefined";
+      if (callback && callback.length<1) throw "Callback missing at least 1 placeholder argument";
+      return callback;
+    }
+
+    function fillArray(data,qty) {
+      var array  = [];
+      for (var i=qty; i>0; i--) array.push(data);
+      return array;
+    }
+
+    window.jQQ = {
+      isReady: false,
+      isolate: function() {
+        var callback = validateCallback(arguments[0]);
+        if(!window.jQQ.isReady) return callbacks.push(callback);
+        return callback.apply(this, fillArray(jq, callback.length));
+      },
+
+      setup: function(version) {
+        // Wait for document to load...
+        if (!document.body) {
+          window.onload = function() {
+            window.jQQ.setup(version);
+          };
+          return;
+        }
+        var url = '//ajax.googleapis.com/ajax/libs/jquery/'+ (version || '1.2.6') +'/jquery.min.js';
+        loadScript(url, function() {
+          window.jQQ.isReady = true;
+          // this stores the new version and gives back the old one, completely.
+          jq = jQuery.noConflict(true);
+          for (var thisCallback in callbacks) {
+            window.jQQ.isolate(callbacks[thisCallback]);
+          }
+          callbacks = {};
+        });
+      }
+    };
+  })(window.jQuery, window.$);
+}
+
+jQQ.setup();
